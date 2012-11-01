f9aec92f830396a7ab255997c79750f81db94cab
ileana89 <ileana_princess_1010@hotmail.com>
2012-11-01 11:52:30 -0600

    proyecto netbeans en blanco

---
+++ options/src/options/Options.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package options;
+
+/**
+ *
+ * @author gataway
+ */
+public class Options
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
